646231af916bbdacf9dc2e4d38d2f5c9e40c1d75
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2023-11-09 17:49:57 +0900

    Title: 0, 1 상태 바꾸기, Time: 212ms, Memory: 73MB, Status: Passed - CodeTree

--- 231109/0, 1 상태 바꾸기/0,-1-change-state.py
+++ 231109/0, 1 상태 바꾸기/0,-1-change-state.py
@@ -1,15 +1,16 @@
-n, m = tuple(map(int,input().split()))
-res = list(map(int,input().split()))
-
-for i in range(m):
-    a, b, c = tuple(map(int,input().split()))
-    if a == 1:
+n, m = map(int, input().split())
+res = list(map(int, input().split()))
+for _ in range(m) :
+    a, b, c = map(int, input().split())
+    if a == 1 :
         res[b-1] = c
-    elif a == 2:
-        for j in range(b-1,c):
-            res[j] = abs(res[j]-1)
-    elif a == 3:
-        res[b-1:c] = 0
-    else:
-        res[b-1:c] = 1
-print(' '.join(map(str,res)))
+    else :
+        for i in range(b-1, c) :
+            if a == 2 :
+                res[i] = 1 - res[i]
+            elif a == 3 :
+                res[i] = 0
+            else :
+                res[i] = 1
+
+print(*res)
