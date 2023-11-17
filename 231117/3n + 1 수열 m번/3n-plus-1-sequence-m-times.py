e144896928c14048b87d80fbeb84cc3b95b95b89
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2023-11-17 09:23:29 +0900

    Title: 3n + 1 수열 m번, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 231117/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -0,0 +1,11 @@
+n = int(input())
+for i in range(n):
+    num = int(input())
+    count = 0
+    while num != 1:
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num = num*3 +1
+        count += 1
+    print(count)
