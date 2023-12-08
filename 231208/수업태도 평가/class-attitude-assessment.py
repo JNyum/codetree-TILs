ee0ee5861dec047738e440239d4b1f1841e30aae
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2023-12-08 10:03:56 +0900

    Title: 수업태도 평가, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 231208/수업태도 평가/class-attitude-assessment.py
+++ 231208/수업태도 평가/class-attitude-assessment.py
@@ -9,9 +9,12 @@ for i in range(n):
     except:
         res[name] = score
 
-value_set = sorted(set(res.values()))
-try:
-    res_key = [k for k, v in res.items() if v == value_set[1]]
-except:
-    res_key = list(res.keys())
-print(res_key[0])
+if len(res) == 1:
+    print(list(res.keys())[0])
+else:
+    value_set = sorted(set(res.values()))
+    if len(value_set) == 1:
+        print('Tie')
+    else:
+        res_key = [k for k, v in res.items() if v == value_set[1]]
+        print(res_key[0])
