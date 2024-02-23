cadbd56d0feda81a191ab571d82abe41b4a81ad2
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2024-02-23 21:21:58 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240223/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,21 @@
+n, m = tuple(map(int,input().split()))
+res = [list(map(int,input().split())) for _ in range(n)]
+
+visited = [[False]*n for _ in range(m)]
+
+def can_go(x,y):
+    return x >= 0 and x < n and y >= 0 and y < m and res[x][y] == 1
+
+def dfs(x,y):
+    dxs, dys= [1,0], [0,1]
+    for dx, dy in zip(dxs,dys):
+        next_x, next_y = x+dx, y+dy
+        if can_go(next_x,next_y) and not visited[next_x][next_y]:
+            visited[next_x][next_y] = True
+            dfs(next_x,next_y)
+
+visited[0][0] = True
+dfs(0,0)
+print(1 if visited[n-1][m-1] else 0)
+# for res_ in res:
+#     print(res_)
