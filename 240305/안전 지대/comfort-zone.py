bc0c3fd44c5712f20f0dd8cede4fa9d23c317977
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2024-03-05 17:59:56 +0900

    Title: 안전 지대, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240305/안전 지대/comfort-zone.py
+++ 240305/안전 지대/comfort-zone.py
@@ -1,71 +1,37 @@
-# n, m = map(int, input().split())
-# res = [list(map(int, input().split())) for _ in range(n)]
-# dxs, dys = [0,1,0,-1], [1,0,-1,0]
+import sys
+sys.setrecursionlimit(10000)
+n, m = map(int, input().split())
+res = [list(map(int, input().split())) for _ in range(n)]
+dxs, dys = [0,1,0,-1], [1,0,-1,0]
 
-# def count_safe(limit):
-#     visited = [[False] * m for _ in range(n)]
-#     count = 0
-
-#     def dfs(x, y):
-#         if x < 0 or x >= n or y < 0 or y >= m:
-#             return
-#         if visited[x][y] or res[x][y] <= limit:
-#             return
-#         visited[x][y] = True
-#         for dx, dy in zip(dxs,dys):
-#             new_x, new_y = x + dx, y + dy
-#             dfs(new_x, new_y)
-
-#     for i in range(n):
-#         for j in range(m):
-#             if not visited[i][j] and res[i][j] > limit:
-#                 dfs(i,j)
-#                 count += 1
-#     return count
-
-# max_val = 0
-# k = 0
-
-# for i in range(1, 101):
-#     safe = count_safe(i)
-#     if safe >= max_val:
-#         max_val = safe
-#         k = i
-
-# print(k, max_val)
-N, M = map(int, input().split())
-heights = [list(map(int, input().split())) for _ in range(N)]
-
-def count_safe_areas(height_limit):
-    visited = [[False] * M for _ in range(N)]
-    safe_area_count = 0
+def count_safe(limit):
+    visited = [[False] * m for _ in range(n)]
+    count = 0
 
     def dfs(x, y):
-        if x < 0 or x >= N or y < 0 or y >= M:
+        if x < 0 or x >= n or y < 0 or y >= m:
             return
-        if visited[x][y] or heights[x][y] <= height_limit:
+        if visited[x][y] or res[x][y] <= limit:
             return
         visited[x][y] = True
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        for dx, dy in directions:
+        for dx, dy in zip(dxs,dys):
             new_x, new_y = x + dx, y + dy
             dfs(new_x, new_y)
 
-    for i in range(N):
-        for j in range(M):
-            if not visited[i][j] and heights[i][j] > height_limit:
-                dfs(i, j)
-                safe_area_count += 1
+    for i in range(n):
+        for j in range(m):
+            if not visited[i][j] and res[i][j] > limit:
+                dfs(i,j)
+                count += 1
+    return count
 
-    return safe_area_count
+max_val = 0
+k = 0
 
-max_safe_areas = 0
-best_K = 0
+for i in range(1, 101):
+    safe = count_safe(i)
+    if safe >= max_val:
+        max_val = safe
+        k = i
 
-for K in range(1, 101):  # K의 범위는 1부터 100까지
-    safe_areas = count_safe_areas(K)
-    if safe_areas >= max_safe_areas:
-        max_safe_areas = safe_areas
-        best_K = K
-
-print(best_K, max_safe_areas)
+print(k, max_val)
