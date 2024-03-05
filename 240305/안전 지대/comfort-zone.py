4b5de2219c8ea4b44cdad7c3b01373bf6cd60b3f
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2024-03-05 18:29:38 +0900

    Title: 안전 지대, Time: 1085ms, Memory: 177MB, Status: Passed - CodeTree

--- 240305/안전 지대/comfort-zone.py
+++ 240305/안전 지대/comfort-zone.py
@@ -1,22 +1,28 @@
 import sys
 sys.setrecursionlimit(10000)
-n, m = map(int, input().split())
-res = [list(map(int, input().split())) for _ in range(n)]
+
+n, m = tuple(map(int,input().split()))
+res = [list(map(int,input().split())) for _ in range(n)]
+# 오 아 왼 위
 dxs, dys = [0,1,0,-1], [1,0,-1,0]
 
-def count_safe(limit):
-    visited = [[False] * m for _ in range(n)]
-    count = 0
+def in_range(x,y):
+    pass
 
-    def dfs(x, y):
-        if x < 0 or x >= n or y < 0 or y >= m:
+
+
+def count_safe(limit):
+    count = 0
+    visited = [[False]*m for _ in range(n)]
+    def dfs(x,y):
+        if x<0 or x>=n or y<0 or y>=m:
             return
         if visited[x][y] or res[x][y] <= limit:
             return
         visited[x][y] = True
         for dx, dy in zip(dxs,dys):
-            new_x, new_y = x + dx, y + dy
-            dfs(new_x, new_y)
+            new_x, new_y = x+dx, y+dy
+            dfs(new_x,new_y)
 
     for i in range(n):
         for j in range(m):
@@ -24,14 +30,9 @@ def count_safe(limit):
                 dfs(i,j)
                 count += 1
     return count
-
-max_val = 0
-k = 0
-
-for i in range(1, 101):
-    safe = count_safe(i)
-    if safe >= max_val:
-        max_val = safe
-        k = i
-
-print(k, max_val)
+counts = []
+for k in range(1,100):
+    counts.append(count_safe(k))
+max_val = max(counts)
+max_k = counts.index(max_val)+1
+print(max_k, max_val)
