8a9832f944cca82917a0476e188e9a2b58b0315b
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2024-03-09 19:18:43 +0900

    Title: 갈 수 있는 곳들, Time: 206ms, Memory: 26MB, Status: Passed - CodeTree

--- 240309/갈 수 있는 곳들/places-can-go.py
+++ 240309/갈 수 있는 곳들/places-can-go.py
@@ -4,28 +4,26 @@ res = [list(map(int,input().split())) for _ in range(n)]
 dots = [tuple(map(int,input().split())) for _ in range(k)]
 # 오 아 왼 위
 dxs, dys = [0,1,0,-1], [1,0,-1,0]
-visited = [[False]*n for _ in range(n)]
 
 def in_range(x,y):
     return 0<=x<n and 0<=y<n
 
 dq = deque()
-res_set = set()
-
-def bfs():
-    while dq:
-        x, y = dq.popleft()
-        visited[x][y] = True
-        for dx, dy in zip(dxs,dys):
-            new_x, new_y = x+dx, y+dy
-            if in_range(new_x,new_y) and not visited[new_x][new_y] and res[new_x][new_y] == 0:
-                dq.append((new_x,new_y))
-                res_set.add((new_x,new_y))
-                # print(res_set)
+res_count = 0
 
 for dot in dots:
-    x, y = dot[0]-1, dot[1]-1
-    res_set.add((x,y))
-    dq.append((x,y))
-    bfs()
-print(len(res_set))
+    x, y = dot[0] - 1, dot[1] - 1
+    if res[x][y] == 0:
+        res_count += 1
+        dq.append((x, y))
+        res[x][y] = 1
+
+while dq:
+    x, y = dq.popleft()
+    for dx, dy in zip(dxs, dys):
+        new_x, new_y = x + dx, y + dy
+        if in_range(new_x, new_y) and res[new_x][new_y] == 0:
+            res_count += 1
+            dq.append((new_x, new_y))
+            res[new_x][new_y] = 1
+print(res_count)
