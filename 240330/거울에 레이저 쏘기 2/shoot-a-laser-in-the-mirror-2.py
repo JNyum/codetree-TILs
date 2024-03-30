5a7c3da047f6561f2291d4879145771b63813496
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2024-03-30 17:36:51 +0900

    Title: 거울에 레이저 쏘기 2, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240330/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240330/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -5,26 +5,26 @@ x = int(input())
 direction_index = ceil(x/n)
 # 1 2 3 4
 dxs, dys = [1,0,-1,0], [0,-1,0,1]
+dxdy = [(1,0),(0,-1),(-1,0),(0,1)]
 def reflect(direction_index,mirror):
     if mirror == '/':
         if direction_index == 1:
-            direction_index = 2
+            return 2
         if direction_index == 2:
-            direction_index = 1
+            return 1
         if direction_index == 3:
-            direction_index = 4
+            return 4
         if direction_index == 4:
-            direction_index = 3
+            return 3
     if mirror == '\\':
         if direction_index == 1:
-            direction_index = 4
+            return 4
         if direction_index == 2:
-            direction_index = 3
+            return 3
         if direction_index == 3:
-            direction_index = 2
+            return 2
         if direction_index == 4:
-            direction_index = 1
-    return direction_index
+            return 1
 def start(x):
     range_x = [i for i in range(n)]
     range_y = [i for i in range(n)]
@@ -39,12 +39,12 @@ def start(x):
     return s_x, s_y
 def in_range(x,y):
     return 0<=x<n and 0<=y<n
-s_x, s_y = start(x)
+x, y = start(x)
 count = 0
-# print(tuple(zip(dxs,dys)))
-while in_range(s_x,s_y):
-    direction = list(zip(dxs,dys))[reflect(direction_index,res[s_x][s_y])-1]
-    s_x += direction[0]
-    s_y += direction[1]
+while in_range(x,y):
+    direction_index = reflect(direction_index,res[x][y])
+    direction = dxdy[direction_index-1]
+    x += direction[0]
+    y += direction[1]
     count += 1
 print(count)
