e61a2d82e7a44bb754a70b97062520e14bfe4bdc
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2024-05-16 19:23:58 +0900

    Title: 이상한 진수 2, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240516/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,11 @@
+n = input()
+num = []
+for i in range(len(n)):
+    num.append(n[i])
+index = 0
+while index < len(n):
+    if num[index] == '0':
+        num[index] = '1'
+        break
+    index += 1
+print(int(''.join(num),2))
