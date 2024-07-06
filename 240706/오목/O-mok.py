5d8f2a952c0f01f619cdbc081cd5a29952b68ff5
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2024-07-06 15:20:23 +0900

    Title: 오목, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240706/오목/O-mok.py
+++ 240706/오목/O-mok.py
@@ -5,25 +5,33 @@ def in_range(x,y):
 
 def row_win(x,y,num):
     for i in range(5):
-        if in_range(x,y+i) and matrix[x][y+i] != num:
+        if not in_range(x,y+i):
+            return False
+        if matrix[x][y+i] != num:
             return False
     return True
 
 def col_win(x,y,num):
     for i in range(5):
-        if in_range(x+i,y) and matrix[x+i][y] != num:
+        if not in_range(x+i,y):
+            return False
+        if matrix[x+i][y] != num:
             return False
     return True
 
 def leftup_win(x,y,num):
     for i in range(5):
-        if in_range(x+i,y+i) and matrix[x+i][y+i] != num:
+        if not in_range(x+i,y+i):
+            return False
+        if matrix[x+i][y+i] != num:
             return False
     return True
 
 def leftdown_win(x,y,num):
     for i in range(5):
-        if in_range(x+i,y-i) and matrix[x+i][y-i] != num:
+        if not in_range(x+i,y-i):
+            return False
+        if matrix[x+i][y-i] != num:
             return False
     return True
 
@@ -39,18 +47,22 @@ def win():
                 if row_win(i,j,num):
                     print(num)
                     print(i+1,j+3)
+                    print(1)
                     return
                 if col_win(i,j,num):
                     print(num)
                     print(i+3,j+1)
+                    print(2)
                     return
                 if leftup_win(i,j,num):
                     print(num)
                     print(i+3,j+3)
+                    print(3)
                     return
                 if leftdown_win(i,j,num):
                     print(num)
                     print(i+3,j-1)
+                    print(4)
                     return
     print(0)
     return
