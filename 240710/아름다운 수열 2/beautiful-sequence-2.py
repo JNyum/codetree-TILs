ba7f48f2cb0a60c6613b920f001a69db37746363
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2024-07-10 20:11:48 +0900

    Title: 아름다운 수열 2, Time: 1287ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240710/아름다운 수열 2/beautiful-sequence-2.py
+++ 240710/아름다운 수열 2/beautiful-sequence-2.py
@@ -4,12 +4,13 @@ n, m = tuple(map(int,input().split()))
 seq = list(map(int,input().split()))
 sub = list(map(int,input().split()))
 
-target = list(permutations(sub,m))
-
 count = 0
-for i in range(n-m+1):
-    if tuple(seq[i:i+m]) in target:
-        # print(tuple(seq[i:i+m]))
-        count += 1
+
+if n >= m:
+    target = list(permutations(sub,m))
+    for i in range(n-m+1):
+        if tuple(seq[i:i+m]) in target:
+            count += 1
+else:
+    pass
 print(count)
-# print(target)
