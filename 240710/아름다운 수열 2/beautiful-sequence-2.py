91a92dd726cb4921679b22dd43cc789767a54ed1
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2024-07-10 20:15:49 +0900

    Title: 아름다운 수열 2, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240710/아름다운 수열 2/beautiful-sequence-2.py
+++ 240710/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,16 +1,45 @@
-from itertools import permutations
+# from itertools import permutations
 
-n, m = tuple(map(int,input().split()))
-seq = list(map(int,input().split()))
-sub = list(map(int,input().split()))
+# n, m = tuple(map(int,input().split()))
+# seq = list(map(int,input().split()))
+# sub = list(map(int,input().split()))
+
+# count = 0
+
+# if n >= m:
+#     target = list(permutations(sub,m))
+#     for i in range(n-m+1):
+#         if tuple(seq[i:i+m]) in target:
+#             count += 1
+# else:
+#     pass
+# print(count)
+
+from collections import Counter
+
+n, m = tuple(map(int, input().split()))
+seq = list(map(int, input().split()))
+sub = list(map(int, input().split()))
 
 count = 0
 
 if n >= m:
-    target = list(permutations(sub,m))
-    for i in range(n-m+1):
-        if tuple(seq[i:i+m]) in target:
+    target_count = Counter(sub)
+    window_count = Counter(seq[:m])
+    
+    if window_count == target_count:
+        count += 1
+    
+    for i in range(1, n - m + 1):
+        window_count[seq[i - 1]] -= 1
+        if window_count[seq[i - 1]] == 0:
+            del window_count[seq[i - 1]]
+        
+        window_count[seq[i + m - 1]] += 1
+        
+        if window_count == target_count:
             count += 1
 else:
     pass
+
 print(count)
