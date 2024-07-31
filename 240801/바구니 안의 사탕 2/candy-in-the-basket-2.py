d9f662818765e62abdac50cc1e665202b1a48f6f
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2024-08-01 01:13:47 +0900

    Title: 바구니 안의 사탕 2, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 240801/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240801/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,12 +1,16 @@
 n, k = tuple(map(int,input().split()))
-buckets = [0]*100
+buckets = [0]*101
 for _ in range(n):
     candies, bucket = tuple(map(int,input().split()))
     buckets[bucket] += candies
 
 max_value = 0
-
-for i in range(k,100-k):
-    value = sum(buckets[i-k:i+k+1])
-    max_value = max(max_value,value)
+if k <= 50:
+    for i in range(k,100-k):
+        value = sum(buckets[i-k:i+k+1])
+        max_value = max(max_value,value)
+else:
+    for i in range(100-k,k):
+        value = sum(buckets[i+k:i-k+1:-1])
+        max_value = max(max_value,value)
 print(max_value)
