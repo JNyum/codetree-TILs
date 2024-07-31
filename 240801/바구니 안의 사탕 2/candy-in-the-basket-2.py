89c6fe2682081195a14cf45e506c841d8c5f4b64
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2024-08-01 01:05:38 +0900

    Title: 바구니 안의 사탕 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240801/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240801/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,41 +1,12 @@
-# n, k = tuple(map(int,input().split()))
-# buckets = {}
-# for _ in range(n):
-#     candy, number = tuple(map(int,input().split()))
-#     buckets[number] = candy
-
-
-# res = []
-# for i in range(k,100-k):
-#     count = 0
-#     for j in range(1,k+1):
-#         try:
-#             count += buckets[i+j]
-#         except: pass
-#         try:
-#             count += buckets[i-j]
-#         except: pass
-#     res.append(count)
-#     count = 0
-# print(max(res))
-
-n, k = tuple(map(int, input().split()))
-buckets = {}
+n, k = tuple(map(int,input().split()))
+buckets = [0]*100
 for _ in range(n):
-    candy, number = tuple(map(int, input().split()))
-    buckets[number] = candy
+    candies, bucket = tuple(map(int,input().split()))
+    buckets[bucket] += candies
 
-max_candies = 0
+max_value = 0
 
-# Iterate over all possible basket numbers
-for i in range(1, 101):
-    current_candies = 0
-    # Check all baskets within k range
-    for j in range(-k, k + 1):
-        if i + j in buckets:
-            current_candies += buckets[i + j]
-    # Update maximum candies found
-    if current_candies > max_candies:
-        max_candies = current_candies
-
-print(max_candies)
+for i in range(k,100-k):
+    value = sum(buckets[i-k:i+k+1])
+    max_value = max(max_value,value)
+print(max_value)
