26a64cb8a5311a846deaac616d7a2092f9ce0d89
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2024-08-07 16:54:10 +0900

    Title: 십자 모양 폭발, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240807/십자 모양 폭발/cross-shape-bomb.py
@@ -0,0 +1,29 @@
+n = int(input())
+matrix = [list(map(int,input().split())) for _ in range(n)]
+x,y = map(lambda x:x-1,tuple(list(map(int,input().split()))))
+
+bomb_range = matrix[x][y]
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+matrix[x][y] = 0
+for i in range(bomb_range):
+    for j in range(bomb_range):
+        if in_range(x+i,y):
+            matrix[x+i][y] = 0
+        if in_range(x, y+j):
+            matrix[x][y+j] = 0
+        if in_range(x-i,y):
+            matrix[x-i][y] = 0
+        if in_range(x, y-j):
+            matrix[x][y-j] = 0
+
+for i in range(n-1,0,-1):
+    for j in range(n):
+        if matrix[i][j] == 0:
+            matrix[i][j] = matrix[i-1][j]
+            matrix[i-1][j] = 0
+
+for matrix_ in matrix:
+    print(' '.join(map(str,matrix_)))
