c2842fcaaef3ebe34769a79854bb7471cf2241a8
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2025-02-13 12:56:35 +0900

    Title: n개의 숫자 중 최소, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250213/n개의 숫자 중 최소/min-of-n-num.py
+++ 250213/n개의 숫자 중 최소/min-of-n-num.py
@@ -3,11 +3,12 @@ nums = list(map(int, input().split()))
 
 # Write your code here!
 min_val = 2**31
-cnt = 0
+cnt = 1
 for num in nums:
     if min_val > num:
         min_val = num
-        if min_val == num:
-            cnt += 1
+        cnt = 1
+    elif min_val == num:
+        cnt += 1
     
 print(min_val,cnt)
