6616d05981212ead40a103e903d0f28826ecfa4d
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2025-02-14 20:17:22 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250214/중복되지 않는 정수 중 최대/max-of-unique-number.py
+++ 250214/중복되지 않는 정수 중 최대/max-of-unique-number.py
@@ -2,10 +2,21 @@ n = int(input())
 nums = list(map(int, input().split()))
 
 # Write your code here!
+# 1. 중복 확인 함수
+duplicate = []
+def is_it_duplicated(num):
+    if num in duplicate:
+        return True
+    else:
+        duplicate.append(num)
+        return False
+
+# 2. 반복문
 max_val = -1
 for num in nums:
-    if num > max_val:
-        max_val = num
-    elif num == max_val:
-        max_val = -1
+    if num >= max_val:
+        if is_it_duplicated(num):
+            max_val = -1
+        else:
+            max_val = num
 print(max_val)
