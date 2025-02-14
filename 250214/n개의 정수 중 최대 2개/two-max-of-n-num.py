efcb37646344471880da5f0be943a039460a075c
Jeongyoon Jang <91577569+JNyum@users.noreply.github.com>
2025-02-14 19:58:31 +0900

    Title: n개의 정수 중 최대 2개, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250214/n개의 정수 중 최대 2개/two-max-of-n-num.py
+++ 250214/n개의 정수 중 최대 2개/two-max-of-n-num.py
@@ -5,9 +5,9 @@ nums = list(map(int, input().split()))
 max_val = -(2**31)
 second_max_val = -(2**31)+1
 for num in nums:
-    if num > second_max_val and num < max_val:
+    if num >= second_max_val and num < max_val:
         second_max_val = num
-    elif num > max_val:
+    elif num >= max_val:
         second_max_val = max_val
         max_val = num
         
